d87b1f1f9d7e477e8e3b63557153760450d8ea33
Tinnekke Bebout <beboutt@pnw.edu>
2019-12-09 20:02:22 -0600

    Create hash2.py

---
+++ hash2.py
@@ -0,0 +1,23 @@
+from Crypto.Hash import MD5
+from Crypto.PublicKey import RSA
+from Crypto import Random 
+
+rng = Random.new().read
+
+RSAKey = RSA.generate(1024, rng)
+
+text1 = open('symmKey.txt', 'r')
+hashtext = text1.read()
+text1.close
+
+hash = MD5.new(hashtext).digest()
+
+#signature = RSAKey.sign(hash, rng)
+text2 = open('symmHash.txt', 'w')
+text2.write(hash)
+text2.close()
+
+
+
+#print RSAKey.verify(hash, signature)
+#print RSAKey.verify(hash[:-1], signature)
